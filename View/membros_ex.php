d2e454ea84c1b00899979ec0c88e8ef2521e068b
MathTav <matheus.amorim03@hotmail.com>
2021-05-18 13:50:20 -0300

    php

---
+++ View/membros_ex.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+include_once("connection.php");
+
+$email = $_SESSION['email'];
+
+$name_member = filter_input(INPUT_POST, 'name_member', FILTER_SANITIZE_STRING);
+$type = $_POST['selectName'];
+$gender = $_POST['selectGender'];
+$fk = "SELECT id FROM users where email = '$email'";
+$pintao = mysqli_query($conn, $fk);
+
+if (!$pintao) {
+    echo 'Could not run query: ';
+    exit;
+}
+$row = mysqli_fetch_row($pintao);
+
+
+
+$result_family = "INSERT INTO membro_familia(nome_membro, tipo_membro, genero_membro, fk_user) VALUES('$name_member', '$type', '$gender', '$row[0]')";
+mysqli_query($conn, $result_family);
+
+if (mysqli_insert_id($conn)){
+    header("Location: cadastro_sucesso.php");
+}else{
+    header("Location: home.php");
+}
+
+
+?>
