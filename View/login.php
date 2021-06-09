559273a1d4efb918522c880abeb5b7a89ea8e96f
Kevin Collinys Silva Walkey <kevinskywalkey@gmail.com>
2021-06-08 21:24:32 -0300

    Login mudança

--- View/login.php
+++ View/login.php
@@ -9,16 +9,7 @@ $sql = "SELECT * FROM users WHERE email = '$email' and password = '$senha'";
 $rss = mysqli_query($conn, $sql);
 
 if ($email && $senha){
-    if (mysqli_num_rows($rss) > 0){
-        $rs = mysqli_fetch_array($rss);
-        session_start();
-        $_SESSION['email'] = $rs["email"];
-        header("Location: index.php");
-    }
-    else{
-        echo "<script> alert('A senha ou email estao errados'); </script> ";
-        echo "<meta http-equiv='refresh' content='1; url=landingpage.php'>";
-    }
+    
 }
 else{
     echo "<script> alert('A senha ou email estao em branco'); </script> ";
