5ec2e2f9963c4147c016e5c27dd55c2454617f04
FernandaCosta244 <79222719+FernandaCosta244@users.noreply.github.com>
2021-06-08 22:30:10 -0300

    att conection

---
+++ conexão.php
@@ -0,0 +1,12 @@
+<?php
+
+$servername = "localhost:3307";
+$username = "root";
+$password = "";
+$database = "PESHI";
+$email = "";
+
+$conn = mysqli_connect($servername, $username, $password, $database,$email);
+
+
+?>
